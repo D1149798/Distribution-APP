27d36e293782ed1fa38e5e02215d3e6c49094b9d
MysteryAuA <a20040226@gmail.com>
2025-05-14 01:28:31 +0800

    chat還沒改fragment

---
+++ app/src/main/java/fcu/app/distributionapp/adapter/ChatAdapter.java
@@ -0,0 +1,89 @@
+package fcu.app.distributionapp.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import fcu.app.distributionapp.R;
+import fcu.app.distributionapp.model.Message;
+
+public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<Message> messages;
+    private String currentUserId = "user_123"; // 假設目前使用者的 ID（之後可以改成從 Firebase auth 取得）
+
+    private static final int VIEW_TYPE_ME = 1;
+    private static final int VIEW_TYPE_OTHER = 2;
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Message msg = messages.get(position);
+        return msg.getSenderId().equals(currentUserId) ? VIEW_TYPE_ME : VIEW_TYPE_OTHER;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_ME) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message_self, parent, false);
+            return new MyMessageViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message, parent, false);
+            return new OtherMessageViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Message msg = messages.get(position);
+        if (holder instanceof MyMessageViewHolder) {
+            ((MyMessageViewHolder) holder).bind(msg);
+        } else if (holder instanceof OtherMessageViewHolder) {
+            ((OtherMessageViewHolder) holder).bind(msg);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages != null ? messages.size() : 0;
+    }
+
+    static class MyMessageViewHolder extends RecyclerView.ViewHolder {
+        TextView messageText;
+
+        public MyMessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            messageText = itemView.findViewById(R.id.tv_message);
+        }
+
+        public void bind(Message msg) {
+            messageText.setText(msg.getText());
+        }
+    }
+
+    static class OtherMessageViewHolder extends RecyclerView.ViewHolder {
+        TextView messageText;
+
+        public OtherMessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            messageText = itemView.findViewById(R.id.tv_message);
+        }
+
+        public void bind(Message msg) {
+            messageText.setText(msg.getText());
+        }
+    }
+}
